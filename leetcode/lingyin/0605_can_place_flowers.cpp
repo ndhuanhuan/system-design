75361c673701592a020c77a63397c5de7321ae23
Di <ndhuanhuan@Dis-MacBook-Air.local>
2026-01-12 23:30:31 -0800

    add lc questions

---
+++ leetcode/lingyin/0605_can_place_flowers.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/can-place-flowers/description/
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int left = n;
+        if (left == 0) return true;
+        
+        for (int i = 0; i < flowerbed.size(); ++i) {
+            if ((i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size()-1 || flowerbed[i+1] == 0) && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                left--;
+            }
+
+            if (left == 0) return true;
+        }
+
+        return left == 0;
+    }
+};

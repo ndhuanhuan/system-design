805ad67ac2cdb15acae1e8b3528caedfde0fc976
Di <ndhuanhuan@Dis-MacBook-Air.local>
2026-01-13 22:37:45 -0800

    add lc questions

---
+++ leetcode/lingyin/0636_exclusive_time_of_functions.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/exclusive-time-of-functions/description/
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> res(n, 0);
+        int curr;
+        int fid;
+        char action[6];
+        int prev;
+        stack<int> st;
+
+        for (auto& log: logs) {
+            sscanf(log.c_str(), "%d:%[a-z]:%d", &fid, action, & curr);
+            if (action[0] == 's') {
+                if (!st.empty()) {
+                    res[st.top()] += curr - prev;
+                }
+
+                st.push(fid);
+                prev = curr;
+            } else {
+                int f = st.top();
+                st.pop();
+                res[f] += curr - prev + 1;
+                prev = curr + 1;
+            }
+        }
+
+        return res;
+    }
+};

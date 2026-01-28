23b41d35aef6d9bea4d6ffd85dbb69e1369cd477
Di <ndhuanhuan@Dis-MacBook-Air.local>
2026-01-27 16:37:27 -0800

    fix

--- leetcode/lingyin/0256_paint_house.cpp
+++ leetcode/lingyin/0256_paint_house.cpp
@@ -16,3 +16,21 @@ public:
         return min({dp[n-1][0], dp[n-1][1], dp[n-1][2]});
     }
 };
+
+
+// my version
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        const int n = costs.size();
+        vector<vector<int>> dp(n+1, vector<int>(3, 0));
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                dp[i][j] = min(dp[i-1][(j+1)%3], dp[i-1][(j+2)%3]) + costs[i-1][j];
+            }  
+        }
+
+        return min({dp[n][0], dp[n][1], dp[n][2]});
+    }
+};
